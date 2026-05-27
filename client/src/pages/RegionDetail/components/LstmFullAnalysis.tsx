a9c75e3f49e080ce68168cdc735872a538d81f99
김은석 <kakkumok2009@gmail.com>
2026-05-28 02:40:29 +0900

    브런치 병합 후 중복 import 항목 제거

--- client/src/pages/RegionDetail/components/LstmFullAnalysis.tsx
+++ client/src/pages/RegionDetail/components/LstmFullAnalysis.tsx
@@ -25,8 +25,6 @@ import {
 import type { ChartData, ChartDataset } from 'chart.js';
 import { Line, Doughnut } from 'react-chartjs-2';
 import type { AptComplex, LstmAnalysis, ArimaAnalysis, ConfidenceDataScope } from '../../../types/region-detail';
-import type { ChartData } from 'chart.js';
-import type { AptComplex, LstmAnalysis, ArimaAnalysis } from '../../../types/region-detail';
 import { useThemeStore } from '../../../stores/useThemeStore';
 
 // Chart.js 전역 등록 (한 번만)
